7149338bc2481234ae18ca566c6f5fd200c69228
Kasper Engelen <kasper.engelen@hotmail.com>
2025-11-18 20:08:46 +0100

    Adjusted the solver runner to also plot the step-sizes.

--- num_ode/NumOdeTutorial/src/SolverRunner.jl
+++ num_ode/NumOdeTutorial/src/SolverRunner.jl
@@ -5,28 +5,45 @@ include("./ODELib.jl")
 include("./IVPSolver.jl")
 
 """
-    solveAndPlotGeneric(;ivp::InitialValueProblem, solvers::Array{IVPSolver}, filename::String)
+    solveAndPlot(;ivp::InitialValueProblem, solvers::Array{IVPSolver}, filenamePrefix::String)
 
     Solve the specified IVP using each of the specified IVP solvers and plot the resulting trajectories. 
-    Each trajectory will be labeled with the label specified by the respective solver.
-    The resulting plot will be stored at the specified filename.
+    Each trajectory will be labeled with the label of the respective solver.
+    
+    All filenames of the plots will be named with the specified filename prefix. There will be a "traj" plot
+    with the simulated trajectories, and a "steps" plot with the step-sizes.
 """
-function solveAndPlot(;ivp::InitialValueProblem, solvers::Array{IVPSolver}, filename::String)
-    # plot for each solver
-    for solver in solvers
-        functionValues = solver.solver(ivp)
+function solveAndPlot(;ivp::InitialValueProblem, solvers::Array{IVPSolver}, filenamePrefix::String)
 
-        if length(functionValues) <= 100
-            plot!(functionValues, markershape = :auto, label=solver.name, dpi=500, title=ivp.name)
-        else
-            # plot no markers if there are too many points
-            plot!(functionValues, markershape = :none, label=solver.name, dpi=500, title=ivp.name)
-        end
-    end
+    # run each solver
+    trajectories = [solver.solver(ivp) for solver in solvers]
 
+    # plot exact solution
     if ivp.exactSolution !== nothing
         # if an exact solution exists, we plot it
         plot!(ivp.exactSolution, ivp.initialTime, ivp.endTime, label="Exact solution", dpi=500)
     end
-    savefig(filename)
+
+    # plot for each solver
+    for (idx, solver) in enumerate(solvers)
+        trajectory = trajectories[idx]
+
+        if length(trajectory) <= 100
+            plot!(trajectory, markershape = :auto, label=solver.name, dpi=500, title="$(ivp.name): trajectories")
+        else
+            # plot no markers if there are too many points
+            plot!(trajectory, markershape = :none, label=solver.name, dpi=500, title="$(ivp.name): trajectories")
+        end
+    end
+    savefig("$(filenamePrefix)_traj.png")
+
+    plot()  # this creates a new empty plot
+    for (idx, solver) in enumerate(solvers)
+        trajectory = trajectories[idx]
+        timeValues = [step[1] for step in trajectory]
+        
+        stepSizes = diff(timeValues)
+        plot!(timeValues[1:end-1], stepSizes, markershape = :none, yaxis=:log10, label=solver.name, dpi=500, title="$(ivp.name): step-sizes")
+    end
+    savefig("$(filenamePrefix)_steps.png")
 end
